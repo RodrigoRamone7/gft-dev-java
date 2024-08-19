f8d89b008f513bac5f5fc970510b5b517bb5401e
RodrigoRamone7 <rodrigo_wow7@hotmail.com>
2024-08-19 18:04:43 -0300

    Initial commit

---
+++ src/test/java/dio/gft/GftDevJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.gft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GftDevJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
